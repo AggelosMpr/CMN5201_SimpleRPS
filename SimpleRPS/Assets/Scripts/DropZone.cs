b25cf0f374959fbad002e43ce3389e80b4b22437
AggelosMpr <13-12795@saeinstitute.edu>
2019-12-16 21:36:46 +0200

    ZonesLayout
    Need Better Field Layout-Card Skins

---
+++ SimpleRPS/Assets/Scripts/DropZone.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropZone : MonoBehaviour,IDropHandler//,IPointerEnterHandler,IPointerExitHandler
+{
+    [SerializeField]
+    Image TransformToPaper;
+    [SerializeField]
+    Sprite Paper;
+    [SerializeField]
+    Image TransformToScissors;
+    [SerializeField]
+    Sprite Scissors;
+    [SerializeField]
+    Image TransformToRock;
+    [SerializeField]
+    Sprite Rock;
+
+    /*public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        
+
+
+    }
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {       
+
+
+    }*/
+    public void OnDrop(PointerEventData pointerEventData)
+    {       
+        if (this.CompareTag("Field"))
+        {
+            
+            isDragging d= pointerEventData.pointerDrag.GetComponent<isDragging>();
+            //d.StartingZone = this.transform;
+           
+            
+            if (d.CompareTag("ToPaper"))
+            {
+                TransformToPaper.sprite = Paper;
+                d.StartingZone = this.transform.parent.parent;
+            }
+            else if (d.CompareTag("ToScissors"))
+            {
+                TransformToScissors.sprite = Scissors;
+                d.StartingZone = this.transform.parent.parent;
+            }
+            else if (d.CompareTag("ToRock"))
+            {
+                TransformToRock.sprite = Rock;
+                d.StartingZone = this.transform.parent.parent;
+            }
+        }
+    }
+}
---
+++ SimpleRPS/Assets/Scripts/isDragging.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class isDragging : MonoBehaviour, IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+    public Transform StartingZone = null;
+    public void OnBeginDrag(PointerEventData cardTransform)
+    {
+        StartingZone = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+    public void OnDrag(PointerEventData cardTransform)
+    {
+        this.transform.position = cardTransform.position;
+    }
+    public void OnEndDrag(PointerEventData cardTransform)
+    {
+        this.transform.SetParent (StartingZone);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
