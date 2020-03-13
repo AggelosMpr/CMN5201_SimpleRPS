bb6fe504a94c02ad0530d9555fed06a1fb0e02e1
AggelosMpr <13-12795@saeinstitute.edu>
2020-03-13 02:35:05 +0200

    Menu

---
+++ SimpleRPS/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+  
+    public void quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+    public void startApp()
+    {
+        SceneManager.LoadScene("Board");
+        PlayerPrefs.SetInt("enemyPoints", 0);
+        PlayerPrefs.SetInt("myPoints", 0);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void EndGame()
+    {
+        SceneManager.LoadScene("End");
+    }
+}
+
