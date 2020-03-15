40a169bde8a311d28884dc7090aa6ad3a6c1417e
George <13-12738@saeinstitute.edu>
2020-03-15 16:18:14 +0200

    Menu

---
+++ SimpleRPS/Assets/Scripts/InfoMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoMenu : MonoBehaviour
+{
+    public GameObject InfoPanel;
+
+    bool isActive;
+
+    public void onInfoButtonClicked()
+    {
+        if (isActive)
+        {
+            InfoPanel.SetActive(true);
+            isActive = false;
+        }
+        else
+        {
+            InfoPanel.SetActive(false);
+            isActive = true;
+        }
+
+
+    }
+}
